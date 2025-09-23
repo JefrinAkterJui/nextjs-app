8eaabc2c56b7d1e9eade94fb2176461c51e6af2c
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-09-23 17:20:07 +0600

    Add: Optimize Fonts, Improve SEO with Metadata, and Use Import Aliases

--- src/app/(commonLayout)/contact/page.tsx
+++ src/app/(commonLayout)/contact/page.tsx
@@ -1,5 +1,8 @@
+import { Metadata } from "next"
 
-
+export const metadata: Metadata={
+  title:"Contact"
+}
 const ContactPage = () => {
   return (
     <div className="text-4xl">Contact Page</div>
--- src/app/(commonLayout)/gallery/page.tsx
+++ src/app/(commonLayout)/gallery/page.tsx
@@ -1,6 +1,10 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
 import React from 'react'
 
+export const metadata: Metadata={
+  title:"Gallery"
+}
 const GalleryPage = () => {
   return (
     <div>
