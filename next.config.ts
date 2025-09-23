765797b52b39e293162cfc1e3b63d3c8c2528739
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-09-23 12:28:19 +0600

    Image Optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqUYlc456G70wXkzfSb2YKWF_vZLTzslEZpg&s')],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/(commonLayout)/gallery/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <div>
+        <h1 className='text-4xl text-center'>App Imge</h1>
+        {/* <img 
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqUYlc456G70wXkzfSb2YKWF_vZLTzslEZpg&s" alt="next.js" 
+            width={500}
+            height={500}
+            className='mx-auto'
+        /> */}
+        <Image
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqUYlc456G70wXkzfSb2YKWF_vZLTzslEZpg&s" alt="next.js" 
+            width={500}
+            height={500}
+            className='mx-auto'
+        />
+    </div>
+  )
+}
+
+export default GalleryPage
