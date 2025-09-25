4c4cf69a626ea86be5308ade8d2be65b5763ecae
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-09-25 11:50:51 +0600

    Breaking the Limits of Static Sites with Next.js ISR

--- src/app/(commonLayout)/product/page.tsx
+++ src/app/(commonLayout)/product/page.tsx
@@ -15,7 +15,9 @@ export interface ProductCardProps {
 
 const ProductPage = async () => {
   const res = await fetch("http://localhost:5000/products",{
-    cache:"force-cache"
+    next:{
+      revalidate: 5
+    }
   });
   const products: ProductCardProps['product'][] = await res.json();
 
