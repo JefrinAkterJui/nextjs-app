197ccd435f8c440cd382e6eed44682c413c68470
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-09-26 11:02:39 +0600

    Handling Active Links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,71 @@
-
+"use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
     <div className="">
-        <nav className=" flex items-center justify-between bg-blue-950 text-xl text-white py-2 px-8">
-            <div><Link href={"/"}>Next.js</Link></div>
-            <div className="flex gap-3">
-                <Link href="/about" className="">About</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/gallery">Gallery</Link>
-                <Link href="/product">Products</Link>
-                <Link href="/dashboard" className="">Dashboard</Link>
-            </div>
-        </nav>
+      <nav className=" flex items-center justify-between bg-blue-950 text-xl text-white py-2 px-8">
+        <div>
+          <Link href={"/"}>Next.js</Link>
+        </div>
+        <div className="flex gap-3">
+          <Link
+            href="/about"
+            className={`${
+              pathName === "/about"
+                ? "text-blue-600 font-semibold underline"
+                : "text-white hover:underline"
+            }`}
+          >
+            About
+          </Link>
+          <Link
+            href="/contact"
+            className={`${
+              pathName === "/contact"
+                ? "text-blue-600 font-semibold underline"
+                : "text-white hover:underline"
+            }`}
+          >
+            Contact
+          </Link>
+          <Link
+            href="/gallery"
+            className={`${
+              pathName === "/gallery"
+                ? "text-blue-600 font-semibold underline"
+                : "text-white hover:underline"
+            }`}
+          >
+            Gallery
+          </Link>
+          <Link
+            href="/product"
+            className={`${
+              pathName === "/product"
+                ? "text-blue-600 font-semibold underline"
+                : "text-white hover:underline"
+            }`}
+          >
+            Products
+          </Link>
+          <Link
+            href="/dashboard"
+            className={`${
+              pathName === "/dashboard"
+                ? "text-blue-600 font-semibold underline"
+                : "text-white hover:underline"
+            }`}
+          >
+            Dashboard
+          </Link>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
